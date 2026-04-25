91c5e772629b30ddb56858853296c6a36eed2264
Rajendra Chozhan <rajchozhan024@gmail.com>
2026-04-25 14:57:19 +0530

    Commit after fixes

--- src/test/java/pages/Homepage_Fab.java
+++ src/test/java/pages/Homepage_Fab.java
@@ -1,275 +1,137 @@
 package pages;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
-import basepackage.*;
-
-import org.openqa.selenium.JavascriptExecutor;
-
+import basepackage.BaseClass;
 import org.openqa.selenium.*;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-
-import basepackage.BaseClass;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Homepage_Fab extends BaseClass{
+public class Homepage_Fab extends BaseClass {
+
 	WebDriver driver;
+	WebDriverWait wait;
 
-		public Homepage_Fab(WebDriver driver) {
-			this.driver = driver;
-			PageFactory.initElements(driver, this);  // ONLY THIS
-		}
-
-
-	@FindBy(xpath = "//button[@class='border-0 bg-transparent']//*[name()='svg']")
-	public  WebElement ProfileButton;
-	public void clickProfileIcon()
-	{
-		ProfileButton.click();
+	// ✅ Constructor (ONLY initialization)
+	public Homepage_Fab(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		PageFactory.initElements(driver, this);
 	}
 
+	// ================= LOGIN =================
+
+	@FindBy(xpath = "//button[@class='border-0 bg-transparent']//*[name()='svg']")
+	WebElement profileButton;
+
+	public void clickProfileIcon() {
+		wait.until(ExpectedConditions.elementToBeClickable(profileButton)).click();
+	}
 
 	@FindBy(xpath = "//button[normalize-space()='Login using password']")
-	public  WebElement LoginButton;
+	WebElement loginButton;
 
 	public void clickLogin() {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-		WebElement loginBtn = wait.until(
-				ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Login']"))
-		);
-
-		loginBtn.click();
-	}
-	@FindBy(xpath = "//input[@id='logiemail']")
-	public WebElement EmailField;
-	public void enterEmail(String email)
-	{
-		highLighterMethod(driver,EmailField);
-		EmailField.sendKeys(email);
+		wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
 	}
 
-	@FindBy(xpath = "//input[@id='logipass']")
-	public WebElement Password;
+	@FindBy(id = "logiemail")
+	WebElement emailField;
 
-	public void enterPassword(String password)
-	{
-		highLighterMethod(driver,Password);
-		Password.sendKeys(password);
+	public void enterEmail(String email) {
+		wait.until(ExpectedConditions.visibilityOf(emailField));
+		highLighterMethod(driver, emailField);
+		emailField.sendKeys(email);
 	}
 
-	@FindBy(xpath = "//button[@id='send-otp']")
-	public  WebElement loginButton;
+	@FindBy(id = "logipass")
+	WebElement passwordField;
 
+	public void enterPassword(String password) {
+		wait.until(ExpectedConditions.visibilityOf(passwordField));
+		highLighterMethod(driver, passwordField);
+		passwordField.sendKeys(password);
+	}
 
+	@FindBy(id = "send-otp")
+	WebElement submitLogin;
+
+	public void clickSubmitLogin() {
+		wait.until(ExpectedConditions.elementToBeClickable(submitLogin)).click();
+	}
+
+	// ================= ACCOUNT =================
 
 	@FindBy(xpath = "//button[@class='border-0 bg-transparent']//*[name()='svg']")
-	public  WebElement AccountButton;
-	public void clickAccountIcon() throws InterruptedException {
+	WebElement accountButton;
+
+	@FindBy(xpath = "//img[@src='../../../assets/images/Store Locatorsvg.svg']")
+	WebElement locationIcon;
+
+	public void clickAccountIcon() {
 		Actions act = new Actions(driver);
-		act.moveToElement(Location_ICON).perform();
-		act.moveToElement(AccountButton).perform();
-		wait(6000);
-		AccountButton.click();
+		act.moveToElement(locationIcon).perform();
+		act.moveToElement(accountButton).perform();
+		wait.until(ExpectedConditions.elementToBeClickable(accountButton)).click();
 	}
+
+	// ================= LOGOUT =================
 
 	@FindBy(xpath = "//button[normalize-space()='Log Out']")
-	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	WebElement logoutButton;
 
-	public  WebElement logoutButton =  wait.until(
-			ExpectedConditions.elementToBeClickable(
-			By.xpath("//*[contains(text(),'Log Out')]")));
-
-	public void clicklogout() throws InterruptedException {wait(200);
-		logoutButton.click();
+	public void clicklogout() {
+		wait.until(ExpectedConditions.elementToBeClickable(logoutButton)).click();
 	}
 
-	
-	
-	
-	@FindBy(xpath= "//*[@id=\"CC-loginHeader-logout\"]")
-	WebElement SIGNOUT;
+	// ================= UI VALIDATIONS =================
 
-	
-	public void clicksignout() {
-		highLighterMethod(driver,SIGNOUT);
-		SIGNOUT.click();
-		
-	
-		}
-
-
-	@FindBy(xpath= "//img[@alt='FabIndia']")
-	WebElement FABINDIA_ICON;
+	@FindBy(xpath = "//img[@alt='FabIndia']")
+	WebElement fabIndiaIcon;
 
 	public void verify_FabIndiaicon() {
-		FABINDIA_ICON.isDisplayed();
+		wait.until(ExpectedConditions.visibilityOf(fabIndiaIcon));
+		fabIndiaIcon.isDisplayed();
 	}
 
-
-	@FindBy(xpath= "//div[@class='col-7 search-col']//i[@class='fa-solid fa-magnifying-glass search-bar-icon']")
-	WebElement SEARCHICON;
+	@FindBy(xpath = "//div[contains(@class,'search')]//i[contains(@class,'magnifying-glass')]")
+	WebElement searchIcon;
 
 	public void verify_Searchicon() {
-		SEARCHICON.isDisplayed();
+		wait.until(ExpectedConditions.visibilityOf(searchIcon));
+		searchIcon.isDisplayed();
 	}
 
-	public void searchicon() {
-			SEARCHICON.click();
-		}
-
-	@FindBy(xpath= "//img[@src='../../../assets/images/Store Locatorsvg.svg']")
-	WebElement Location_ICON;
-
-	public void verify_FabLocationicon() {
-		Location_ICON.isDisplayed();
-	}
-
-	@FindBy(xpath= "//a[@class='wishlist_icon_header']")
-	WebElement Wishlist_ICON;
+	@FindBy(xpath = "//a[@class='wishlist_icon_header']")
+	WebElement wishlistIcon;
 
 	public void verify_Wishlisticon() {
-		Wishlist_ICON.isDisplayed();
+		wait.until(ExpectedConditions.visibilityOf(wishlistIcon));
+		wishlistIcon.isDisplayed();
 	}
 
-	@FindBy(xpath= "//a[@aria-label='0 items currently in your cart']//*[name()='svg']")
-	WebElement Cart_ICON;
+	@FindBy(xpath = "//a[contains(@aria-label,'cart')]")
+	WebElement cartIcon;
 
 	public void verify_Carticon() {
-		Cart_ICON.isDisplayed();
+		wait.until(ExpectedConditions.visibilityOf(cartIcon));
+		cartIcon.isDisplayed();
 	}
 
-	@FindBy(xpath= "//div[@class='cx-notice']")
-	WebElement CopyRights_text;
+	@FindBy(xpath = "//div[@class='cx-notice']")
+	WebElement copyRightsText;
 
-	public void verify_CopyRightsText() throws InterruptedException {
-
-
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", CopyRights_text);
-		Thread.sleep(500);
-		CopyRights_text.isDisplayed();
+	public void verify_CopyRightsText() {
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", copyRightsText);
+		wait.until(ExpectedConditions.visibilityOf(copyRightsText));
+		copyRightsText.isDisplayed();
 	}
 
-
-	public void VerifyProfileIcon()
-	{
-		ProfileButton.isDisplayed();
+	public void verify_FabLocationicon() {
+		wait.until(ExpectedConditions.visibilityOf(locationIcon));
+		locationIcon.isDisplayed();
 	}
-	
-	@FindBy(id= "CC-headerWidget-Search")
-	WebElement SEARCHBAR;
-	
-	public void searchbar() {
-		highLighterMethod(driver,SEARCHBAR);
-		SEARCHBAR.click();
-		SEARCHBAR.clear();
-		SEARCHBAR.sendKeys("KURTA FOR MEN");
-		
-		}
-	
-	
-	@FindBy(xpath= "/html/body/div[3]/header/div/div/div/div/div[1]/div/div[1]/div/div/div/div[2]/div[4]/div/div/div/div/div/form/div[1]/button/span[2]/img")
-	WebElement CLICKSEARCH;
-	
-	public void clicksearch() {
-		
-		CLICKSEARCH.click();
-		
-		}
-	
-	
-	
-	
-	
-	
-	@FindBy(xpath= "//p[contains(text(),'Linen Dobby Weave Long Kurta')]")
-	WebElement LinenDobbyWeaveLongKurta;
-	
-
-	public void clickkurta() {
-		
-
-	    highLighterMethod(driver,LinenDobbyWeaveLongKurta);
-		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS) ;
-
-	    LinenDobbyWeaveLongKurta.click();
-		
-		}
-	
-	
-
-	@FindBy(xpath= "//body/div[@id='page']/main[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[4]/div[2]/div[4]/div[2]/div[1]/div[1]/ul[1]/li[3]/button[1]")
-	WebElement SELECT_M_SIZE;
-	
-	
-public void selectMsize() throws InterruptedException  {
-		
-		highLighterMethod(driver,SELECT_M_SIZE);
-		
-		SELECT_M_SIZE.click();
-		
-		}
-
-
-@FindBy(xpath= "//body/div[@id='page']/main[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[4]/div[2]/div[6]/div[2]/div[1]/button[1]")
-WebElement ADDTOCART;
-
-
-public void addtocart()  {
-	
-	highLighterMethod(driver,ADDTOCART);
-	ADDTOCART.click();
-}
-
-
-@FindBy(xpath= "//body[1]/div[3]/header[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/img[1]")
-WebElement CLOSEICON;
-
-
-public void closeicon()  {
-	
-	highLighterMethod(driver,CLOSEICON);
-	CLOSEICON.click();
-}
-
-@FindBy(xpath= "//header/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/img[1]")
-WebElement CARTICON;
-
-
-public void carticon()  {
-	
-	highLighterMethod(driver,CARTICON);
-	CARTICON.click();
-}
-
-
-
-
-
-@FindBy(xpath= "//*[@id=\"cat100137\"]")
-WebElement FOOD;
-
-public void movetofood() throws InterruptedException {
-	
-	highLighterMethod(driver,FOOD);
-	Actions action = new Actions(driver);
-	action.moveToElement(FOOD).build().perform();
-	Thread.sleep(3000);
-	
-}
-
-public void food()  {
-	
-	highLighterMethod(driver,FOOD);
-	FOOD.click();
-}
-
-
 }
